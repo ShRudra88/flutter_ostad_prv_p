0988a36460812e7723c9fc48c8e1e138a6c2ffe2
ShRudra88 <salhawathosenrudra503@gmail.com>
2023-10-09 13:19:39 +0600

    form in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,110 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
+    return MaterialApp(
+      title: 'Simple Form App',
+      home: MyForm(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
+class MyForm extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  _MyFormState createState() => _MyFormState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _MyFormState extends State<MyForm> {
+  final GlobalKey<FormBuilderState> _formKey = GlobalKey<FormBuilderState>();
+  final ValueNotifier<bool> autovalidate = ValueNotifier<bool>(false);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        title:  Text("Homepage"),
-        backgroundColor: Colors.black12,
+        title: Text('Simple Form App'),
       ),
-      backgroundColor: Colors.pinkAccent,
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                // Your action here
-              },
-              child: Text('Elevated Button'),
-            ),
-            SizedBox(height: 20),
-            TextButton(
-              onPressed: () {
-                // Your action here
-              },
-              child: Text('Text Button'),
-            ),
-            SizedBox(height: 20),
-            OutlinedButton(
-              onPressed: () {
-                // Your action here
-              },
-              child: Text('Outlined Button'),
-            ),
-            SizedBox(height: 20),
-            IconButton(
-              onPressed: () {
-                // Your action here
-              },
-              icon: Icon(Icons.add),
-            ),
-            SizedBox(height: 20),
-            FloatingActionButton(
-              onPressed: () {
-              },
-              child: Icon(Icons.add),
-              backgroundColor: Colors.black12,
-            )
-            /* DropdownButton<String>(
-              items: <String>['Option 1', 'Option 2', 'Option 3']
-                  .map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-              onChanged: (String newValue) {
-                // Handle dropdown selection
-              },
-            )
-
-            Radio(
-              value: selectedValue,
-              groupValue: groupValue,
-              onChanged: (value) {
-                setState(() {
-                  groupValue = value;
-                });
-              },
-            )
-
-            Switch(
-              value: isSwitched,
-              onChanged: (value) {
-                setState(() {
-                  isSwitched = value;
-                });
-              },
-            )
-            */
-
-          ],
+        padding: EdgeInsets.all(20.0),
+        child: FormBuilder(
+          key: _formKey,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          child: Column(
+            children: <Widget>[
+              FormBuilderTextField(
+                name: 'name',
+                decoration: InputDecoration(labelText: 'Name'),
+                validator: FormBuilderValidators.required(context),
+              ),
+              SizedBox(height: 20),
+              FormBuilderTextField(
+                name: 'email',
+                decoration: InputDecoration(labelText: 'Email'),
+                validator: FormBuilderValidators.compose([
+                  FormBuilderValidators.required(context),
+                  FormBuilderValidators.email(context),
+                ]),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.saveAndValidate()) {
+                    final formData = _formKey.currentState!.value;
+                    // Handle the form data (e.g., submit it to a server)
+                    print(formData);
+                  } else {
+                    autovalidate.value = true;
+                  }
+                },
+                child: Text('Submit'),
+              ),
+            ],
+          ),
         ),
-      )
-      );
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    autovalidate.dispose();
+    super.dispose();
   }
 }
